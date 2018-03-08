caaea1bd882e1ca4727bec3561baf68e1027120a
RohitR <roy.rohit08@gmail.com>
2018-03-07 21:29:41 -0500

    Created objects of second(2,zero) and third(5,2) test class

--- src/FirstFiles/GitTestFirstClass.java
+++ src/FirstFiles/GitTestFirstClass.java
@@ -2,5 +2,12 @@ class GitTestFirstClass{
 	public static void main(String[] args) {
 		System.out.println("GitTestFirstClass");
 		System.out.println("Hello GIT!");
+
+		GitTestSecondClass gTSC1 = new GitTestSecondClass(2, "zero");
+		int val = gTSC1.getA();
+		System.out.println("value of A is " + val);
+
+		GitTestThirdClass gTTC1 = new GitTestThirdClass(5, val);
+		System.out.println("SUM is " + gTTC1.calculateSumThirdClass());
 	}	
 }
